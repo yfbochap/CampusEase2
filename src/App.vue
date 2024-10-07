ad40dd52963ee3bb3bbd61613554a2a347123fac
YF Leong <yfbochap@gmail.com>
2024-10-08 00:55:05 +0800

    test 2

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="CampusEase" />
+      <HelloWorld msg="CampusEase is Up" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
