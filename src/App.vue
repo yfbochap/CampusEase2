5bd8d9d269382f09050386d8b910b606d1856e1c
YF Leong <yfbochap@gmail.com>
2024-10-16 17:23:35 +0800

    resolved conflict

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="CampusEase 1234!" />
+      <HelloWorld msg="CampusEase!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/dashboard">Dashboard</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+
+
+</script>
+
+<template>
+    <div style="background-color: black;">
+        <h2 style="color:white;">
+            CampusEase
+        </h2>
+    </div>
+</template>
+
